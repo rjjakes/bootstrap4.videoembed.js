0a8555b0c498aa05ba7a86bc26d587eddd54df69
Ray <rayjamesjakes@gmail.com>
2017-12-17 17:58:28 +0000

    Webpack and fetch polyfill.

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+    entry: ['whatwg-fetch', './source/bootstrap4.videoembed.js'],
+    output: {
+        path: path.resolve(__dirname, 'distribution'),
+        filename: 'bootstrap4.videoembed.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    }
+}
